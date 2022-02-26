d57ef7ba7b24e6370ed09e99d79628f6ce1945c6
dqj5182 <dqj5182@psu.edu>
2022-02-27 02:29:16 +0900

    commit

--- train_cifar10.py
+++ train_cifar10.py
@@ -3,9 +3,7 @@ from __future__ import print_function
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.nn.functional as F
 import torch.backends.cudnn as cudnn
-import numpy as np
 
 import torchvision
 import torchvision.transforms as transforms
@@ -20,13 +18,13 @@ from randomaug import RandAugment
 
 # parsers
 parser = argparse.ArgumentParser(description='PyTorch CIFAR10 Training')
-parser.add_argument('--lr', default=1e-3, type=float, help='learning rate') # resnets.. 1e-3, Vit..1e-4?
+parser.add_argument('--lr', default=1e-4, type=float, help='learning rate') # resnets.. 1e-3, Vit..1e-4?
 parser.add_argument('--opt', default="adam")
 parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
 parser.add_argument('--aug', action='store_true', help='use randomaug')
 parser.add_argument('--amp', action='store_true', help='enable AMP training')
 parser.add_argument('--mixup', action='store_true', help='add mixup augumentations')
-parser.add_argument('--net', default='vit')
+parser.add_argument('--net', default='vit_timm')
 parser.add_argument('--bs', default='4')
 parser.add_argument('--n_epochs', type=int, default='50')
 parser.add_argument('--patch', default='4', type=int)
@@ -38,7 +36,6 @@ args = parser.parse_args()
 import gc
 gc.collect()
 torch.cuda.empty_cache()
-
 
 # take in args
 watermark = "{}_lr{}".format(args.net, args.lr)
@@ -57,6 +54,7 @@ start_epoch = 0  # start from epoch 0 or last checkpoint epoch
 print('==> Preparing data..')
 if args.net=="vit_timm":
     size = 384
+
 transform_train = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
     transforms.Resize(size),
@@ -86,7 +84,7 @@ classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship'
 
 # Model
 print('==> Building model..')
-# net = VGG('VGG19')
+
 if args.net=="vit_timm":
     import timm
     net = timm.create_model("vit_large_patch16_384", pretrained=True)
